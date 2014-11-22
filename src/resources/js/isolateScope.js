6104c7011c802758c26a57312dbf835221dff6ec
K.C. Hunter <kchunter@connectionseducation.com>
2014-11-22 14:17:39 -0500

    Part 10 video files added.

--- src/resources/js/isolateScope.js
+++ src/resources/js/isolateScope.js
@@ -3,10 +3,14 @@ var app = angular.module("isolateApp", []);
 
 app.controller("AppCtrl", function ($scope, $element)
 {
-	$scope.useMove = function (obj)
+	$scope.getMove = function (name, movetype, move)
 	{
-		console.log(obj);
+		console.log('' + name + ' performed a ' + movetype +
+			' ' + move );
 	}
+
+	$scope.movetypes 	= ['Finisher', 'Offensive Move', 'Defensive Move'];
+	$scope.movetype 	= $scope.movetypes[0];
 })
 
 .directive("character", function ()
@@ -15,8 +19,11 @@ app.controller("AppCtrl", function ($scope, $element)
 		restrict: 'E',
 		scope: {
 			name: "@",
-			image: "@"
+			image: "@",
+			movetype: "=",
+			useMove: '&'
 		},
-		templateUrl: 'partials/shield_isolate.html'
+		templateUrl: 'partials/shield_isolate.html',
+		controller: 'AppCtrl'
 	}
 })
